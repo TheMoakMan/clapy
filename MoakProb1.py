3782c9c36d7ddef09e7d4d34fc765b10c357f187
Evan <evan@DESKTOP-G1L2GK1.localdomain>
2020-03-12 02:28:20 -0400

    Outline

--- MoakProb1.py
+++ MoakProb1.py
@@ -9,14 +9,14 @@ class Matrix():
 
         print(row, col, matrix)
 
+        # Check if matrix dim are equal to specified dimensions
         if row != len(matrix):
-            raise DimError
+            raise DimError(DIM_ERR)
     
         for n in range(row):
          
             if type(matrix[n]) != list or len(matrix[n]) != col:
-                raise DimError
-
+                raise DimError(DIM_ERR)
 
         # Default to matrix of all 0's
         if self.arr == []:
@@ -24,6 +24,42 @@ class Matrix():
                 self.arr += [[0]]
                 for _ in range(self.shape[1] - 1):
                     self.arr[i] += [0]
+
+    # Adjusting matrix
+
+    # Length of Vector
+
+    # Transpose of Matrix
+    
+    # Conjugate of Matrix
+    
+    # Adjoint of Matrix
+    
+    # Matrix Multiplication
+    
+    # Scalar Multiplication (int or float) * matrix  
+
+    # Print out entire matrix
+    # def show(self)
+
+    # bracket operators? so you don't have to call .arr for []
+
+    # Basis
+
+    # Inner Product of Matrices
+
+    # Kroneker Product
+
+
+
+
+
+
+
+
+
+
+        
 
         
 
--- errors.py
+++ errors.py
@@ -1,5 +1,4 @@
 DIM_ERR = "Input matrix does not match specified dimensions."
 
 class DimError(Exception):
-    def __init__(self, msg=DIM_ERR):
-        pass
+    pass
